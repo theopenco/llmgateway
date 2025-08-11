249b415e21c5ae56124dec93ffb71b3b825931f1
Luca Steeb <contact@luca-steeb.com>
2025-08-12 00:14:20 +0200

    fix(db): correct processed_at update logic
    Updated the `processed_at` column to use `created_at` for unprocessed logs instead of
    `NOW()` to reflect the correct processing time.

--- packages/db/migrations/1754922211_lowly_absorbing_man.sql
+++ packages/db/migrations/1754922211_lowly_absorbing_man.sql
@@ -1,4 +1,4 @@
 ALTER TABLE "log" ADD COLUMN "processed_at" timestamp;
 
 -- Set all existing logs as processed since they would have been processed by the old system
-UPDATE "log" SET "processed_at" = NOW() WHERE "processed_at" IS NULL;
+UPDATE "log" SET "processed_at" = "created_at" WHERE "processed_at" IS NULL;
