5f9f288e3fca6b93f7c41d943f69e5c3b7544277
Luca Steeb <contact@luca-steeb.com>
2025-08-13 02:22:42 +0200

    Revert "feat(db): add processed_at column to log table"
    This reverts commit adccf75c27b1ea63fde9b9f2012323721886c743.

--- packages/db/migrations/1754922211_lowly_absorbing_man.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "log" ADD COLUMN "processed_at" timestamp;
-
--- Set all existing logs as processed since they would have been processed by the old system
-UPDATE "log" SET "processed_at" = NOW() WHERE "processed_at" IS NULL;
