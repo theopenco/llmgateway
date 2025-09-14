39ca972d74914f0b569017af308ac61f0ea0c99e
Luca Steeb <contact@luca-steeb.com>
2025-09-15 00:06:28 +0100

    chore: migrations

--- packages/db/migrations/1757891174_pale_mercury.sql
+++ packages/db/migrations/1757891174_pale_mercury.sql
@@ -47,6 +47,22 @@ CREATE TABLE "model_provider_mapping" (
 	CONSTRAINT "model_provider_mapping_modelId_providerId_unique" UNIQUE("model_id","provider_id")
 );
 --> statement-breakpoint
+CREATE TABLE "model_provider_mapping_history" (
+	"id" text PRIMARY KEY NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"model_id" text NOT NULL,
+	"provider_id" text NOT NULL,
+	"minute_timestamp" timestamp NOT NULL,
+	"logs_count" integer DEFAULT 0 NOT NULL,
+	"errors_count" integer DEFAULT 0 NOT NULL,
+	"error_rate" real DEFAULT 0 NOT NULL,
+	"throughput" real DEFAULT 0 NOT NULL,
+	"total_output_tokens" integer DEFAULT 0 NOT NULL,
+	"total_duration" integer DEFAULT 0 NOT NULL,
+	CONSTRAINT "model_provider_mapping_history_modelId_providerId_minuteTimestamp_unique" UNIQUE("model_id","provider_id","minute_timestamp")
+);
+--> statement-breakpoint
 CREATE TABLE "provider" (
 	"id" text PRIMARY KEY NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
@@ -68,4 +84,6 @@ CREATE TABLE "provider" (
 );
 --> statement-breakpoint
 ALTER TABLE "model_provider_mapping" ADD CONSTRAINT "model_provider_mapping_model_id_model_id_fk" FOREIGN KEY ("model_id") REFERENCES "public"."model"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "model_provider_mapping" ADD CONSTRAINT "model_provider_mapping_provider_id_provider_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."provider"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "model_provider_mapping" ADD CONSTRAINT "model_provider_mapping_provider_id_provider_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."provider"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "model_provider_mapping_history" ADD CONSTRAINT "model_provider_mapping_history_model_id_model_id_fk" FOREIGN KEY ("model_id") REFERENCES "public"."model"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "model_provider_mapping_history" ADD CONSTRAINT "model_provider_mapping_history_provider_id_provider_id_fk" FOREIGN KEY ("provider_id") REFERENCES "public"."provider"("id") ON DELETE cascade ON UPDATE no action;
