b1128c2b7bd36922c4eb3b9bdf6add27e3f9abd9
Luca Steeb (bot) <contact@luca-steeb.com>
2025-09-15 16:50:41 +0000

    chore(autofix): apply diff

---
+++ packages/db/migrations/1757955010_sour_dreaming_celestial.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "model" DROP COLUMN "error_rate";--> statement-breakpoint
+ALTER TABLE "model_provider_mapping" DROP COLUMN "error_rate";--> statement-breakpoint
+ALTER TABLE "provider" DROP COLUMN "error_rate";
