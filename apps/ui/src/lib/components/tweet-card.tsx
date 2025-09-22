d87459d2a1d546c809ce0b9ae1078d59f2635653
smakosh <ismai23l@hotmail.com>
2025-09-22 20:30:59 +0200

    fix: quick condition check

--- apps/ui/src/lib/components/tweet-card.tsx
+++ apps/ui/src/lib/components/tweet-card.tsx
@@ -120,10 +120,9 @@ export const TweetHeader = ({ tweet }: { tweet: EnrichedTweet }) => (
 					className="flex items-center whitespace-nowrap font-semibold"
 				>
 					{truncate(tweet.user.name, 20)}
-					{tweet.user.verified ||
-						(tweet.user.is_blue_verified && (
-							<Verified className="ml-1 inline size-4 text-blue-500" />
-						))}
+					{(tweet.user.verified || tweet.user.is_blue_verified) && (
+						<Verified className="ml-1 inline size-4 text-blue-500" />
+					)}
 				</a>
 				<div className="flex items-center space-x-1">
 					<a
