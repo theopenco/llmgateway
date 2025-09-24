3ddb7fc5dc4ca12ac04a2cae2753237a799b4359
Luca Steeb <contact@luca-steeb.com>
2025-09-24 23:13:26 +0100

    chore: eslint fix

--- apps/playground/eslint.config.mjs
+++ apps/playground/eslint.config.mjs
@@ -1,6 +1,5 @@
 import lint from "../../eslint.config.mjs";
 import react from "@abinnovision/eslint-config-react";
-import noRelativeImportPathsPlugin from "eslint-plugin-no-relative-import-paths";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
