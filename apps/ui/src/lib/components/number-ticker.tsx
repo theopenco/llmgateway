d00d5d7b851f4649376d64458829d12da2073ee9
smakosh <ismai23l@hotmail.com>
2025-12-23 02:10:54 +0100

    fix(number-ticker): ensure cleanup function is defined in useEffect

--- apps/ui/src/lib/components/number-ticker.tsx
+++ apps/ui/src/lib/components/number-ticker.tsx
@@ -39,6 +39,8 @@ export function NumberTicker({
 			}, delay * 1000);
 			return () => clearTimeout(timer);
 		}
+
+		return () => {};
 	}, [motionValue, isInView, delay, value, direction, startValue]);
 
 	useEffect(
