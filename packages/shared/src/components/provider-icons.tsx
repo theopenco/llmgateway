bfb1c882c601314da2d91223293c50ce320808ad
Luca Steeb <contact@luca-steeb.com>
2026-01-13 17:06:50 +0000

    chore: icon

--- packages/shared/src/components/provider-icons.tsx
+++ packages/shared/src/components/provider-icons.tsx
@@ -109,10 +109,6 @@ export const BytedanceIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
 ) => (
 	<svg
 		height="1em"
-		style={{
-			flex: "none",
-			lineHeight: 1,
-		}}
 		viewBox="0 0 24 24"
 		width="1em"
 		xmlns="http://www.w3.org/2000/svg"
