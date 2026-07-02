6b1f281dbcdcc7a5b42b85c98b7720483627bfe4
Luca Steeb <contact@luca-steeb.com>
2026-07-02 22:19:12 +0100

    chore: migrations

--- packages/db/migrations/1783008677_spooky_rhino.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "user_organization" ADD COLUMN "max_api_keys" integer;--> statement-breakpoint
-ALTER TABLE "user_organization" ADD COLUMN "usage_limit" numeric;--> statement-breakpoint
-ALTER TABLE "user_organization" ADD COLUMN "period_usage_limit" numeric;--> statement-breakpoint
-ALTER TABLE "user_organization" ADD COLUMN "period_usage_duration_value" integer;--> statement-breakpoint
-ALTER TABLE "user_organization" ADD COLUMN "period_usage_duration_unit" text;
