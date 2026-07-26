d88b9290634dfda231637fbd1625f3362f4a666a
Luca Steeb <contact@luca-steeb.com>
2026-07-26 20:43:18 +0100

    chore: reset migrations before merging main

--- packages/db/migrations/1785088647_charming_clint_barton.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "provider_key" ADD COLUMN "sort_order" integer;--> statement-breakpoint
-CREATE INDEX "provider_key_sort_order_idx" ON "provider_key" ("organization_id","provider","sort_order");
