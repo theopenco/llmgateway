4507190a4a1d727d85102263505cc41a3297536e
Luca Steeb <contact@luca-steeb.com>
2026-08-16 23:38:04 +0200

    chore(db): reset migrations to main before merge

--- packages/db/migrations/1786566890_striped_morlocks.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "organization" ADD COLUMN "sso_default_projects_configured" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "organization" ALTER COLUMN "sso_default_projects_configured" SET DEFAULT true;
