426ba3fb071fd3bd8cdc7e6cbfe9172a3882d2d9
Luca Steeb <contact@luca-steeb.com>
2026-08-29 23:53:06 +0200

    chore(db): reset migrations before sync

--- packages/db/migrations/1787926837_nasty_princess_powerful.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "provider_company" ADD COLUMN "website_verification_token" text;--> statement-breakpoint
-ALTER TABLE "provider_company" ADD COLUMN "website_verified_domain" text;--> statement-breakpoint
-ALTER TABLE "provider_company" ADD COLUMN "website_verified_at" timestamp;--> statement-breakpoint
-ALTER TABLE "provider_draft_model" ADD COLUMN "rate_limit_scope" text DEFAULT 'global' NOT NULL;
