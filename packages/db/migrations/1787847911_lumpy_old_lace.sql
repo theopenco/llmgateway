34069c316b08a791560d4c4f60cefc5d092a69f8
Luca Steeb <contact@luca-steeb.com>
2026-09-01 15:48:57 +0900

    chore(db): reset migrations before main merge

--- packages/db/migrations/1787847911_lumpy_old_lace.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "organization" ADD COLUMN "subscription_payment_status" text DEFAULT 'current' NOT NULL;--> statement-breakpoint
-UPDATE "organization"
-SET "subscription_payment_status" = 'past_due'
-WHERE "dev_plan_credits_frozen" = true;
