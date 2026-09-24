46437733c44e5be8b68efafafd8a7cf04f571979
Luca Steeb <contact@luca-steeb.com>
2026-09-24 18:45:04 +0700

    chore(db): reset migrations before syncing main

--- packages/db/migrations/1790168318_lonely_master_chief.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "provider_claim" ADD COLUMN "verification_key_ciphertext" text;--> statement-breakpoint
-ALTER TABLE "provider_claim" ADD COLUMN "verification_key_masked" text;--> statement-breakpoint
-ALTER TABLE "provider_claim" ADD COLUMN "verification_key_updated_at" timestamp;
